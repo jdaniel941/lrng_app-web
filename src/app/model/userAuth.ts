89595d138cf819a560f253c594665a8a6c9c537a
Daniel J <d.janus@moodup.team>
2022-02-14 01:07:21 +0100

    auth service

---
+++ src/app/model/userAuth.ts
@@ -0,0 +1,12 @@
+export interface LoginUserData {
+  username: string;
+  password: string;
+}
+
+export interface RegisterUserData {
+  firstName: string;
+  lastName: string;
+  userType: string;
+  email: string;
+  password: string;
+}
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,9 +1,19 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { environment } from "../../environments/environment";
+import { RegisterUserData, LoginUserData } from "../model/userAuth";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class AuthService {
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  login(userData: LoginUserData) {
+    return this.http.post(`${environment.api}/login`, userData);
+  }
+
+  register(userData: RegisterUserData) {
+    return this.http.post(`${environment.api}/register`, userData);
+  }
 }
